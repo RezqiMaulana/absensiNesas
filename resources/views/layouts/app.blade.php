aebc8c0ad11cca5fb9d8d3970bf52901c2d48ada
unknown <mohamadechaputragmail.com>
2026-02-03 18:55:56 +0700

    import hash di import

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
 <body @class(['bg-slate-50', 'dark:bg-slate-900', 'transition-colors', 'duration-300'])>
     <div @class(['flex', 'min-h-screen', 'relative'])>
